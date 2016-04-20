a672227adbf8dd8fa31eedba46e6231990de4cd0
John Velis <john@johnvelis.com>
2016-04-20 05:32:49 -0400

    initial commit

---
+++ Demo_NMM.EF.D5/Models/IdentityRoleTemp.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo_NMM.EF.D2.Models
+{
+    public class IdentityRoleTemp
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public IdentityRoleTemp() { }
+
+        public IdentityRoleTemp(IdentityRoleTemp role)
+        {
+            this.Name = role.Name;
+        }
+    }
+}
